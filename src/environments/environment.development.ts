7e52f69bca81dea7945be275c4421e767875b200
daniloza12 <87085703+daniloza12@users.noreply.github.com>
2026-03-06 15:27:25 -0500

    dashboard logins

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://vitalinkbk-ewd2fufafze3ayay.canadacentral-01.azurewebsites.net/api/v1',
+  apiUrl: 'https://backendvitalink-dthsh0gedtfeemad.canadacentral-01.azurewebsites.net/api/v1',
   //apiUrl: 'http://localhost:8080/api/v1',
 };
 
